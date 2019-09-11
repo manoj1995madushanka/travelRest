6515ed1ed360d74ccc436b1d7e1aaff53d470c73
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-09-11 21:15:08 +0530

    Initial commit

---
+++ src/main/java/com/sunTravel/travelRest/TravelRestApplication.java
@@ -0,0 +1,13 @@
+package com.sunTravel.travelRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelRestApplication.class, args);
+	}
+
+}
